e3699c4968d64ce2e44acae61e638c8e669daf86
maddy89 <madawa911@gmail.com>
2018-07-11 17:54:53 +0900

    Adding modules and reusable comps

--- examples/00-basics.elm
+++ examples/00-basics.elm
@@ -87,3 +87,88 @@ type alias User =
 hasBio : User -> Bool
 hasBio user =
   String.length user.bio > 0
+
+
+-- Union Types - Whenever you have weird shaped data in Elm, you want to reach for a union type
+
+type Visibility = All | Active | Completed
+
+keep : Visibility -> List Task -> List Task
+keep visibility tasks =
+  case visibility of
+    All ->
+      tasks
+
+    Active ->
+      List.filter (\task -> not task.complete) tasks
+
+    Completed ->
+      List.filter (\task -> task.complete) tasks
+
+-- to enforce a value but keep an unassigned value as well:
+type User = Anonymous | Named String
+
+> Anonymous
+Anonymous : User
+
+> Named
+<function> : String -> User
+
+-- usage:
+Anonymous
+Named "AzureDiamond"
+Named "abraham-lincoln"
+
+
+-- Reuseable views
+
+view : Model -> Html Msg
+view model =
+  fieldset []
+    [ label []
+        [ input [ type_ "checkbox", onClick ToggleNotifications ] []
+        , text "Email Notifications"
+        ]
+    , label []
+        [ input [ type_ "checkbox", onClick ToggleAutoplay ] [] -- repeating
+        , text "Video Autoplay"
+        ]
+    , label []
+        [ input [ type_ "checkbox", onClick ToggleLocation ] [] -- repeating
+        , text "Use Location"
+        ]
+    ]
+
+-- after converting into a reusable checkbox:
+checkbox: msg -> String -> Html msg
+
+checkbox msg name = 
+label []
+        [ input [ type_ "checkbox", onClick msg ] []
+        , text name
+        ]
+
+view model =
+  fieldset []
+    [ label []
+        [ checkbox ToggleNotifications "Email Notifications"]
+    , label []
+        [ checkbox ToggleAutoplay "Video Autoplay"]
+    , label []
+        [ checkbox ToggleLocation "Use Location"]
+    ]
+
+
+-- Modules
+
+-- defining module
+module Optional exposing (..) 
+-- module code
+
+-- importing module
+import Optional
+
+
+import Optional as Opt
+
+
