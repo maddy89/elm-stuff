d7a16388216b676c72bda9ae389068e80aeb0d4a
Maddy <madhawa.bandara@rakuten.com>
2018-09-11 17:08:26 +0900

    Add more comments

--- examples/00-basics.elm
+++ examples/00-basics.elm
@@ -1,7 +1,49 @@
 -- Functions
 
+
+-- In this case the signature is saying: Given an integer (Int) as argument return another integer.
+addOne : Int -> Int --signature
+addOne x = --definition
+  x + 1
+addOne 3 --call
+
+
+-- A function that takes no arguments is a constant in Elm:
+name =
+  "Sam"
+
  String.length
 -- <function> : String -> Int, must take a string arg and return int
+
+-- in Elm all functions take exactly one argument and return a result. This result can be another function. Let's explain this using the function above.
+-- When we do:
+
+divide 5 2
+
+-- This is evaluated as:
+
+((divide 5) 2)
+
+-- First `divide 5` is evaluated.
+-- The argument `5` is applied to `divide`, resulting in an intermediate function.
+
+divide 5 -- -> intermediate function
+
+-- Let's call this intermediate function `divide5`.
+-- If we could see the signature and body of this intermediate function, it would look like:
+
+divide5 : Float -> Float
+divide5 y =
+  5 / y
+
+-- So we have a function that has the `5` already applied.
+
+-- Then the next argument is applied i.e. `2`
+
+divide5 2
+
+-- And this returns the final result
+
 
 
 -- Annonymous functions
@@ -54,6 +96,20 @@ eval steps:
 
 -} 
 -- Chaining 
+
+
+add 1 (multiply 2 3)
+sum (filter (isOver 100) (map getCost records))
+
+3
+    |> multiply 2
+    |> add 1
+
+records
+    |> map getCost
+    |> filter (isOver 100)
+    |> sum
+
 -- e.g.
 -- <|
 f x == f <| x 
@@ -67,6 +123,19 @@ ngon 5 30
   |> filled blue
   |> move (10,10)
   |> scale 2
+
+
+-- type variables - like generics - can pass any type
+
+indexOf : String -> List String -> Int -- enforces to pass a list of strings only
+
+indexOf : a -> List a -> Int -- can pass any list as long as types match
+
+
+
+
+
+
 
 
 -- Type aliasing - use an alias to represent complex data types
--- examples/01-button.elm
+++ examples/01-button.elm
@@ -1,64 +1,98 @@
+module Main exposing (Model, Msg(..), main, model, update, view)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
-
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = updatex
+        }
 
 
 
 -- MODEL
-
 -- create an alias Model for type Int
-type alias Model = Int
+
+
+type alias Model =
+    Int
 
 
 model : Model
 model =
-  0
+    0
 
 
 
 -- UPDATE
-
 -- messages we get from the UI
+
+
 type Msg
-  = Increment
-  | Decrement
-  | Reset
+    = Increment
+    | Decrement
+    | Reset
+
+
+
+-- B) if I want to increment by whatever number?
+
+
+type Msg
+    = Increment Int
+    | Decrement Int
+    | Reset
+
+
 
 -- what to do when you receive those messages
-update : Msg -> Model -> Model -- definition of signature, when inputting msg and model, return a model
+
+
+updatex :
+    Msg
+    -> Model
+    -> Model -- definition of signature, when inputting msg and model, return a model
+
+
 
 -- function definition. function_name - update, input_params - msg, model, what to do and return
+
+
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model
 
-    Decrement ->
-      model - 1
+        -- B) Increment + value -> model
+        Decrement ->
+            model - 1
 
-    Reset ->
-       0
+        Reset ->
+            0
 
 
 
 -- VIEW
 -- html DOM
 -- func signature name : input -> output  - when input something of type Model, Html chunk that takes in a Msg and out put html
+
+
 view : Model -> Html Msg
-view model = 
--- htmlnode [attributes] [child nodes]
-  div [] 
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    , Html.br [] []
-    , button [ onClick Reset ] [ text "reset" ]
-    ]
+view model =
+    -- htmlnode [attributes] [child nodes]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+
+        -- B)[ button [ onClick <| Decrement 23 ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , Html.br [] []
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
+
+
+
+--B) represents the another scenario of declaring a type with a param
